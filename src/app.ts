ea8fca42c664d801efb94cc7f0609cd04ab02547
Kevin FEDYNA <fedyna.kevin@gmail.com>
2024-09-18 09:25:37 +0200

    Changed file name to cache.ts, added all CRUD methods and added declaration file for types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import GameRouter from "./router/game-router.js"
-import Cache from "./handler/database.js";
+import Cache from "./handler/cache.js";
 
 const app = express();
 const port = 5050;
 
 app.use("/game", GameRouter);
-
-app.get("/cache", (_, res) => {
-  res.send(Cache.$instance);
-})
 
 Cache.createCategory("game");
 
