d6352b424ec61a35a6f6261bd9168e333a86e754
kasimaru13 <kasimaru13@gmail.com>
2024-11-04 15:38:58 +0100

    opencv init + start aruco

---
+++ handler/src/public/static/script/opencv/board-analysis.js
@@ -0,0 +1,9 @@
+function onCvReady(){
+    cv.then((cv) => {
+        const params = new cv.aruco_DetectorParameters();
+        const dictionary = cv.getPredefinedDictionary(cv.DICT_4X4_100);
+        const refinedParam = new cv.aruco_RefineParameters(10, 3, true)
+        const detector = new cv.aruco_ArucoDetector(dictionary, params, refinedParam);
+        console.log(detector)
+    });
+}
