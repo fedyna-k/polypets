19f34e4f2f5c448df67085d263c2f2bf07f30db6
kasimaru13 <kasimaru13@gmail.com>
2024-11-20 08:45:02 +0100

    image proc fixes

--- handler/src/public/static/script/opencv/ImageProcessor.js
+++ handler/src/public/static/script/opencv/ImageProcessor.js
@@ -10,8 +10,8 @@ class ImageProcessor {
 
         this.params = new this.cv.aruco_DetectorParameters();
         this.dictionary = this.cv.getPredefinedDictionary(this.cv.DICT_4X4_100);
-        this.refinedParam = new this.cv.aruco_RefineParameters(10, 3, true)
-        this.detector = new this.cv.aruco_ArucoDetector(dictionary, params, refinedParam);
+        this.refinedParam = new this.cv.aruco_RefineParameters(10, 3, true);
+        this.detector = new this.cv.aruco_ArucoDetector(this.dictionary, this.params, this.refinedParam);
     }
 
     setCV(opencvObject) {
@@ -24,11 +24,11 @@ class ImageProcessor {
     }
 
     analyseImage(image) {
-        let corners = [];
+        let markers_position = [];
         let ids = [];
         let rejected = [];
-        this.detector.detectMarkers(image, this.dictionary, corners, ids, rejected);
+        this.detector.detectMarkers(image, this.dictionary, markers_position, ids, rejected);
 
-        return [corners, ids];
+        return [markers_position, ids];
     }
 }
