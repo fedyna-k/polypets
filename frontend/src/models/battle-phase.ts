207d22223c6e1032a7372bb72daf5955c05bf54d
Artho <loicganne83@gmail.com>
2024-12-04 12:01:00 +0100

    Started Phase implementation

--- frontend/src/models/battle-phase.ts
+++ frontend/src/models/battle-phase.ts
@@ -1,4 +1,6 @@
+import { BattleTurnLog } from "./battle-turn-log.js";
 import { GamePhase } from "./game-phase.js";
+import { Pet } from "./pet.js";
 
 /**
  * Battle phase class of the game. Automatic phase between two players.
@@ -8,7 +10,13 @@ export class BattlePhase implements GamePhase {
         public game_id: string,
         public id: string,
         public state: string,
-        public start_time: string
+        public start_time: string,
+        public turn_count: number,
+        public max_turn_count: number = 999,
+        public team0: Pet[],
+        public team1: Pet[],
+        public battle_state: string,
+        public history: BattleTurnLog[]
     ) {}
 
     Serialize(): string {
--- frontend/src/models/shop-phase.ts
+++ frontend/src/models/shop-phase.ts
@@ -1,4 +1,6 @@
 import { GamePhase } from "./game-phase.js";
+import { Pet } from "./pet.js";
+import { Food } from "./food.js";
 
 /**
  * Shop phase class of the game. Individual phase.
@@ -8,7 +10,13 @@ export class ShopPhase implements GamePhase {
         public game_id: string,
         public id: string,
         public state: string,
-        public start_time: string
+        public start_time: string,
+        public player_id: string,
+        public money: number,
+        public pets: Pet[],
+        public food: Food[],
+        public available_pets: Pet[],
+        public available_food: Food[]
     ) {}
 
     Serialize(): string {
