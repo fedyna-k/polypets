dfb02e02087b7f14d2af271154b402a832428b50
Kevin FEDYNA <fedyna.kevin@gmail.com>
2025-01-09 20:40:25 +0100

    bug fix on coturn

--- startup.sh
+++ startup.sh
@@ -34,7 +34,10 @@ sudo docker run -it --rm --name certbot \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
             certbot/certbot certonly --standalone -d app.fedyna.fr --text --agree-tos --email fedyna.kevin@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --rsa-key-size 4096 --verbose --keep-until-expiring --preferred-challenges=http
 
+sudo cp /etc/letsencrypt/live/app.fedyna.fr/ ./nginx/certs/dev
+
 /usr/bin/logger "===== STARTING DOCKER COMPOSE ====="
 
 sudo docker compose up --build -d  # Web server
-turnserver --log-file stdout  # TURN server
+sudo systemctl kill coturn
+sudo turnserver --log-file stdout  # TURN server
