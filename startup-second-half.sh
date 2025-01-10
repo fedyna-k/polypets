2faf7612accfdd936ac2ef065fde2f2f19427320
fedyna-k <fedyna.kevin@gmail.com>
2025-01-10 21:29:42 +0100

    Removed -it flag and put letsencrypt in relative folder for easier integration

--- startup-second-half.sh
+++ startup-second-half.sh
@@ -1,12 +1,12 @@
 EOF
 
 # Update certbot certificates
-sudo docker run -it --rm --name certbot \
-            -v "/etc/letsencrypt:/etc/letsencrypt" \
-            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+sudo docker run --rm --name certbot \
+            -v "./etc/letsencrypt:/etc/letsencrypt" \
+            -v "./var/lib/letsencrypt:/var/lib/letsencrypt" \
             certbot/certbot certonly --standalone -d app.fedyna.fr --text --agree-tos --email fedyna.kevin@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --rsa-key-size 4096 --verbose --keep-until-expiring --preferred-challenges=http
 
-sudo cp /etc/letsencrypt/live/app.fedyna.fr/* ./nginx/certs/dev
+sudo cp ./etc/letsencrypt/live/app.fedyna.fr/* ./nginx/certs/dev
 
 sudo docker compose up --build -d  # Web server
 sudo systemctl kill coturn
