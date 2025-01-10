0153bbb5fe06f9352d79c0e5cb80702445aa789b
Kevin FEDYNA <fedyna.kevin@gmail.com>
2025-01-10 22:43:45 +0100

    add tls to turn server

--- startup-3.sh
+++ startup-3.sh
@@ -4,4 +4,6 @@ EOF
 sudo systemctl kill coturn
 
 sudo docker compose up --build -d  # Web server
-sudo turnserver --log-file stdout -u polypets:polypets  # TURN server
+sudo turnserver --log-file stdout -u polypets:polypets \
+  --cert ./nginx/certs/dev/cert.pem \
+  --pkey ./nginx/certs/dev/privkey.pem  # TURN server
