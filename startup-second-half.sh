4b7d3d35bd1bbe52aa4c1e9c3994c965e3bba2b9
fedyna-k <fedyna.kevin@gmail.com>
2025-01-10 21:44:06 +0100

    Enforce :80 availability

--- startup-second-half.sh
+++ startup-second-half.sh
@@ -1,13 +1,21 @@
 EOF
 
+sudo systemctl kill coturn
+sudo systemctl stop nginx
+sudo systemctl stop apache2
+
 # Update certbot certificates
 sudo docker run --rm --name certbot \
             -v "./etc/letsencrypt:/etc/letsencrypt" \
             -v "./var/lib/letsencrypt:/var/lib/letsencrypt" \
-            certbot/certbot certonly --standalone -d app.fedyna.fr --text --agree-tos --email fedyna.kevin@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --rsa-key-size 4096 --verbose --keep-until-expiring --preferred-challenges=http
+            certbot/certbot certonly --non-interactive \
+              --standalone -d app.fedyna.fr \
+              --text --agree-tos --email fedyna.kevin@gmail.com \
+              --server https://acme-v02.api.letsencrypt.org/directory \
+              --rsa-key-size 4096 --verbose --keep-until-expiring \
+              --preferred-challenges=http
 
 sudo cp ./etc/letsencrypt/live/app.fedyna.fr/* ./nginx/certs/dev
 
 sudo docker compose up --build -d  # Web server
-sudo systemctl kill coturn
 sudo turnserver --log-file stdout -u polypets:polypets  # TURN server
